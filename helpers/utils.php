34ef2d3423a299b530b21939ec4b009b2788a149
Luis <luisvillarreal.lgvc@hotmail.com>
2022-10-01 15:33:00 -0500

    Agregando las funciones de ayuda

---
+++ helpers/utils.php
@@ -0,0 +1,96 @@
+<?php 
+class utils{
+	public static function deleteSession($name)	{
+		if (isset($_SESSION[$name])) {
+			$_SESSION[$name] = null;
+			unset($_SESSION[$name]);
+		}
+		return $name;
+	}
+
+	public static function isAdmin(){
+		if (!isset($_SESSION['admin'])) {
+			header('Location:'.base_url);
+		}
+		else{
+			return true;
+		}
+	}
+
+	public static function onlyUser(){
+		$isUser = true;
+		if (isset($_SESSION['admin'])) {
+			$isUser = false;
+		}
+		return $isUser;
+	}
+
+	public static function isIdentity(){
+		if (!isset($_SESSION['identity'])) {
+			header('Location:'.base_url);
+		}
+		else{
+			return true;
+		}
+	}
+
+	public static function showCategories(){
+		require_once 'models/category.php';
+		$category = new category();
+		$categories = $category->getAll();
+		return $categories;
+	}
+
+	public static function showGeneros(){
+		require_once 'models/genero.php';
+		$genero = new genero();
+		$generos = $genero->getAll();
+		return $generos;
+	}
+
+	public static function statsCart(){
+		$stats = array(
+			'count' => 0, 
+			'total' => 0
+		);
+		if(isset($_SESSION['cart'])){
+			$stats['count'] = count($_SESSION['cart']);
+			foreach ($_SESSION['cart'] as $product) {
+				$stats['total'] += $product['price']*$product['amount'];
+			}
+		}
+		return $stats;
+	}
+
+	public static function showStatus($status){
+		$value = 'Pendiente';
+		if ($status == 'Confirm') {
+			$value = 'Pendiente';
+		} elseif ($status == 'Preparation') {
+			$value = 'En preparación';
+		}elseif ($status == 'Ready') {
+			$value = 'Preparado para enviar';
+		}elseif ($status == 'Sended') {
+			$value = 'Enviado';
+		}
+		return $value;
+	}
+
+	public static function hidden(){
+		$hidden = false;
+		if (!isset($_GET['order_id'])) {
+			$hidden = true;
+		}
+		return $hidden;
+	}
+
+	public function getStock($product_id){
+		require_once 'models/product.php';
+		$product = new product();
+		$product->setId($product_id);
+		$product = $product->getOne();
+		return $product->stock;
+	}
+}
+
+?>
