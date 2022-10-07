e0cf03fa107b363f8f29d5bc41ae288aa1faf079
Luis <luisvillarreal.lgvc@hotmail.com>
2022-10-06 20:02:06 -0500

    Configurar rutas

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define('base_url', 'http://localhost/www/Proyecto/Refaccionaria_Autos/');
+define('base_url', 'http://localhost/www/Refaccionaria-MVC/');
 define('controller_default', 'productController');
 define('action_default', 'index');
 ?>
