ee88e0ab8f0833914cef632755e7bd182060e555
Luis <luisvillarreal.lgvc@hotmail.com>
2022-10-06 20:34:20 -0500

    Personalizar error 404

--- controllers/errorController.php
+++ controllers/errorController.php
@@ -1,10 +1,13 @@
-<?php 
+<?php
 /**
- * 
+ *
  */
 class errorController {
-	
+
 	public function index(){
-		echo "<div class='container text-center'><h1>La pagina que buscas no existe</h1></div>";
+		echo "<div class='container text-center'>
+						<h1 class='pt-5 display-1'>404</h1>
+						<h2>Not found</h2>
+					</div>";
 	}
 }
