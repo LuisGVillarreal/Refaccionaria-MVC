9b95bc8ae30e4c89b05c6a4b48879cebfbc1d4ef
Luis Villarreal <78396346+LuisGVillarreal@users.noreply.github.com>
2023-06-23 18:55:37 -0600

    Update parameters.php

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define('base_url', 'https://refaccionariaautos.scm.azurewebsites.net/wwwroot/');
+define('base_url', 'https://refaccionariaautos.azurewebsites.net/');
 define('controller_default', 'productController');
 define('action_default', 'index');
 ?>
