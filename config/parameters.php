8053c4fe459be445187b7f518d5ee4d7a40dc69d
Luis <luisvillarreal.lgvc@hotmail.com>
2023-06-25 21:53:04 -0600

    Config to hostiger

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define('base_url', 'https://refaccionariaautos.azurewebsites.net/');
+define('base_url', 'https://refaccionaria-autos.000webhostapp.com/');
 define('controller_default', 'productController');
 define('action_default', 'index');
 ?>
